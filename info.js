756d1c5f0c1e9057664bc3ab81df9f7712e61310
benjaminjkern <bkern1@lion.lmu.edu>
2022-10-28 09:40:18 -0700

    Hidden player count

--- info.js
+++ info.js
@@ -1,10 +1,13 @@
 var renderInfo;
 window.addEventListener("load", () => {
     renderInfo = (games, players) => {
+        const hiddenPlayers = Object.keys(players)
+            .map((name) => players[name])
+            .filter((player) => player.games < minGames).length;
         document.getElementById("info").innerHTML = `Total games: ${
             games.length
-        }<br>Total players: ${
-            Object.keys(players).length
+        }<br>Total players: ${Object.keys(players).length}${
+            hiddenPlayers ? `(${hiddenPlayers} hidden)` : ""
         }<br>Total money laundered: ${formatMoney(
             games.reduce(
                 (p, game) =>
